9e8a08f0d2a80a6db2dda41a57f191abeacd6bfc
hostilelogout <80536041+hostilelogout@users.noreply.github.com>
2023-03-08 10:24:25 +0100

    Fixed errors regarding annotations
    fixed a curly bracket problem

--- reactapp/src/App.js
+++ reactapp/src/App.js
@@ -1,14 +1,15 @@
 import './App.css';
-import { BrowserRouter,Routes,Route } from 'react-router-dom'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
 
 function App() {
-    return {
-        < BrowserRouter >
-        <div className="App">
-            <Routes>
-            </Routes>
-        </div>
-    }
+    return (
+        <BrowserRouter>
+            <div className="App">
+                <Routes>
+                </Routes>
+            </div>
+        </BrowserRouter>
+    );
 }
 
 export default App;
