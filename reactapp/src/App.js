de4ed4b929ac7c2a70060161ca300f5a78cbfe66
Emil Solgaard <emil.solgaard@dk.experis.com>
2023-03-08 10:55:11 +0100

    feat: added route to /Dashboard view

--- reactapp/src/App.js
+++ reactapp/src/App.js
@@ -1,11 +1,14 @@
 import './App.css';
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 
+import Dashboard from './Views/Dashboard';
+
 function App() {
     return (
         <BrowserRouter>
             <div className="App">
                 <Routes>
+                    <Route path='/Dashboard' element={<Dashboard/>}></Route>
                 </Routes>
             </div>
         </BrowserRouter>
