249ef73a94a9c692e65c7b0f1b06da5900cf1f4a
Farhang Younis <64683703+compsci-78@users.noreply.github.com>
2023-03-09 08:16:26 +0100

    Fix: Changet HttpPut to HttpPatch

--- webapi/Controllers/SetsController.cs
+++ webapi/Controllers/SetsController.cs
@@ -57,7 +57,7 @@ namespace webapi.Controllers
 
         // PUT: api/Sets/5
         // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
-        [HttpPut("{id}")]
+        [HttpPatch("{id}")]
         public async Task<IActionResult> PutSet(int id, SetUpdateDto setUpdateDto)
         {
             if (id != setUpdateDto.Id)
