688d6c414e24298da3081e513676c0f39135b108
Emil Solgaard <emil.solgaard@dk.experis.com>
2023-03-13 16:06:35 +0100

    feat: added "days until goal end" and "completed workouts" to Dashboard "progress"

--- reactapp/src/Views/Dashboard.jsx
+++ reactapp/src/Views/Dashboard.jsx
@@ -6,6 +6,13 @@ import { DashboardContext } from "../Context/DashboardContext";
 
 const Dashboard = () => {
   const [state, setState] = useState({
+    goals: [
+      {id: 1, name: "Goal A", endDate: new Date(2023, 2, 15), workouts: [
+        {id: 1, name: "Workout A", status: "completed"},
+        {id: 2, name: "Workout B", status: "active"}
+      ]},
+      {id: 2, name: "Goal B", endDate: new Date(2023, 3, 1), workouts: []}
+    ],
     selectedGoal: null
   })
 
@@ -14,25 +21,33 @@ const Dashboard = () => {
     setState({...state, selectedGoal: goal})
   }
 
+  const dateDiff = (first, second) => {
+    return Math.ceil((second - first) / (1000 * 60 * 60 * 24));
+  }
+
   return (
     <div className="d-flex flex-column align-items-center p-5">
       <h1>Dashboard</h1>
-      <div className="d-flex wpx-960">
+      <div className="d-flex wpx-960 hpx-480">
 
         {/* GoalList Component */}
         <DashboardContext.Provider value={goalSelected}>
-          <div className="d-flex flex-column align-items-center flex-fill border wp-50 p-2">
+          <div className="d-flex flex-column align-items-center flex-fill border wp-50 hp-100 p-2">
             <p>Goals</p>
             <DashboardContext.Consumer>
               {goalSelected => (
                 <div className="d-flex flex-column flex-fill overflow-y-scroll wp-100">
-                  <button onClick={() => goalSelected({id: 1, name: "Goal A"})}>Goal A</button>
-                  <button onClick={() => goalSelected({id: 2, name: "Goal B"})}>Goal B</button>
+                  {state.goals.map(goal => 
+                    <div key={goal.id} className="d-flex flex-column">
+                      <input onChange={() => goalSelected(goal)} type="radio" name="goal-radio" id={"goal-radio-" + goal.id} className="btn-check"/>
+                      <label htmlFor={"goal-radio-" + goal.id} className="btn btn-outline-secondary">{goal.name}</label>
+                    </div>
+                  )}
                 </div>
               )}
             </DashboardContext.Consumer>
             <Link to="/goals/new">
-              <button>Create goal</button>
+              <button className="btn btn-outline-secondary">Create goal</button>
             </Link>
           </div>
         </DashboardContext.Provider>
@@ -44,7 +59,13 @@ const Dashboard = () => {
               {goal => (
                 <>
                 <p>Progress</p>
-                {goal !== null ? <Link to={"/goals/" + goal.id}>{goal.name}</Link> : "No goal selected.."}
+                {goal !== null ?
+                  <>
+                  <Link to={"/goals/" + goal.id}><p>View goal details</p></Link>
+                  <p>You have {dateDiff(new Date(), goal.endDate)} day(s) to complete goal!</p>
+                  <p>You have completed {goal.workouts.filter(w => w.status === "completed").length} out of {goal.workouts.length} workouts!</p>
+                  </>
+                : "No goal selected.."}
                 </>
               )}
             </DashboardContext.Consumer>
