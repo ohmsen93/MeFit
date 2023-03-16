60d0613646826b23516137ef808a7aefe8d992c2
Emil Solgaard <emil.solgaard@dk.experis.com>
2023-03-16 12:55:04 +0100

    fix: added token bearer to GET exercise

--- reactapp/src/API/ExerciseAPI.jsx
+++ reactapp/src/API/ExerciseAPI.jsx
@@ -1,10 +1,13 @@
+import keycloak from "../keycloak"
+
 // FETCH
 export const fetchExercises = async () => {
     console.log(process.env.REACT_APP_API_URL + "/exercises")
     try {
         const request = await fetch(process.env.REACT_APP_API_URL + "/exercises", {
             headers: {
-                'Access-Control-Allow-Origin': '*'
+                'Access-Control-Allow-Origin': '*',
+                'Authorization': 'Bearer ' + keycloak.token
             }
         })
             .then(response => response.json())
