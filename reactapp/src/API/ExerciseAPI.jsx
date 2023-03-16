b5eadaae76c66f2b5a46c5c5af9b50a1eafa3285
Emil Solgaard <emil.solgaard@dk.experis.com>
2023-03-16 12:47:55 +0100

    feat: exercise list population

---
+++ reactapp/src/API/ExerciseAPI.jsx
@@ -0,0 +1,18 @@
+// FETCH
+export const fetchExercises = async () => {
+    console.log(process.env.REACT_APP_API_URL + "/exercises")
+    try {
+        const request = await fetch(process.env.REACT_APP_API_URL + "/exercises", {
+            headers: {
+                'Access-Control-Allow-Origin': '*'
+            }
+        })
+            .then(response => response.json())
+            .then(results => {
+                return results
+            })
+        return request
+    } catch (error) {
+        console.log(error)
+    }
+}
