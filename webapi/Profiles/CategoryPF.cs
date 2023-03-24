fb7415d3eb599124c44e39d332dc49f916c13af8
Farhang Younis <64683703+compsci-78@users.noreply.github.com>
2023-03-24 14:42:52 +0100

    Feature: Added CRUD endpoints for Categories

---
+++ webapi/Profiles/CategoryPF.cs
@@ -0,0 +1,18 @@
+﻿using AutoMapper;
+using webapi.Models.DTO.CategoryDTO;
+using webapi.Models;
+
+namespace webapi.Profiles
+{
+    public class CategoryPF:Profile
+    {
+        public CategoryPF()
+        {
+            CreateMap<CategoryCreateDto, Category>();
+            CreateMap<Category, CategoryReadDto>()
+                .ForMember(dto => dto.Trainingprograms, options =>
+                options.MapFrom(setDomain => setDomain.Trainingprograms.Select(tp => tp.Id).ToList()));
+            CreateMap<CategoryUpdateDto, Category>();
+        }
+    }
+}
