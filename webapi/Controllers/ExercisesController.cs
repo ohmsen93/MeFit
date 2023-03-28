87c320671068915513a0801db9079a27ea2a9283
Farhang Younis <64683703+compsci-78@users.noreply.github.com>
2023-03-28 09:10:58 +0200

    Feature: Contributor role has been added to the ExercisesController

--- webapi/Controllers/ExercisesController.cs
+++ webapi/Controllers/ExercisesController.cs
@@ -41,6 +41,7 @@ namespace webapi.Controllers
         /// </summary>
         /// <returns></returns>
         [HttpGet]
+        [Authorize(Roles = "Contributor")]
         public async Task<ActionResult<IEnumerable<Exercise>>> GetExercises()
         {
             return Ok(_mapper.Map<ICollection<ExerciseReadDto>>(await _service.GetAll()));
@@ -52,6 +53,7 @@ namespace webapi.Controllers
         /// <param name="id"></param>
         /// <returns></returns>
         [HttpGet("{id}")]
+        [Authorize(Roles = "Contributor")]
         public async Task<ActionResult<Exercise>> GetExercise(int id)
         {
             try
@@ -74,6 +76,7 @@ namespace webapi.Controllers
         /// <param name="exerciseUpdateDto"></param>
         /// <returns></returns>
         [HttpPatch("{id}")]
+        [Authorize(Roles = "Contributor")]
         public async Task<IActionResult> PutExercise(int id, ExerciseUpdateDto exerciseUpdateDto)
         {
             if (id != exerciseUpdateDto.Id)
@@ -104,6 +107,7 @@ namespace webapi.Controllers
         /// <param name="exerciseCreateDto"></param>
         /// <returns></returns>
         [HttpPost]
+        [Authorize(Roles = "Contributor")]
         public async Task<ActionResult<Exercise>> PostExercise(ExerciseCreateDto exerciseCreateDto)
         {
             var exercise = _mapper.Map<Exercise>(exerciseCreateDto);
@@ -119,6 +123,7 @@ namespace webapi.Controllers
         /// <param name="id"></param>
         /// <returns></returns>
         [HttpDelete("{id}")]
+        [Authorize(Roles = "Contributor")]
         public async Task<IActionResult> DeleteExercise(int id)
         {
             try
@@ -143,6 +148,7 @@ namespace webapi.Controllers
         /// <param name="musclegroups"></param>
         /// <returns></returns>
         [HttpPatch("{id}/musclegroups")]
+        [Authorize(Roles = "Contributor")]
         public async Task<ActionResult<Exercise>> PatchExerciseMusclegroups(int id, ExerciseUpdateMusclegroupsDto exerciseUpdateMusclegroupsDto)
         {
             try
@@ -166,6 +172,7 @@ namespace webapi.Controllers
         /// <param name="sets"></param>
         /// <returns></returns>
         [HttpPatch("{id}/sets")]
+        [Authorize(Roles = "Contributor")]
         public async Task<ActionResult<Exercise>> PatchExerciseSets(int id, ExerciseUpdateSetsDto exerciseUpdateSetsDto)
         {
             try
